2ad91ff6d4f346b6f78001a66386866c49214a89
drossano <72749793+drossano@users.noreply.github.com>
2021-11-14 14:34:02 -0500

    Add test for verse 1

--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -23,12 +23,21 @@ class BottlesTest < Minitest::Test
     assert_equal expected, Bottles.new.verse(3)
   end
 
-  def test_verse_2
+  def test_verse2
     expected =
       "2 bottles of beer on the wall, " +
         "2 bottles of beer.\n" +
         "Take one down and pass it around, " +
         "1 bottle of beer on the wall.\n"
     assert_equal expected, Bottles.new.verse(2)
+  end
+
+  def test_verse1
+    expected =
+    n"1 bottle of beer on the wall, " +
+      "1 bottle of beer.\n" +
+      "Take it down and pass it around, " +
+      "o more bottles of beer on the wall.\n"
+    assert_equal expected, Bottles.new.verse(1)
   end
 end
